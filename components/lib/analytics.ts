2ecd25b46544a0bc7576b5c8975cbded9bb19809
mondragon-ai <angel.mondragon@yahoo.com>
2023-05-14 07:40:29 -0500

    landing page

--- components/lib/analytics.ts
+++ components/lib/analytics.ts
@@ -1,6 +1,6 @@
 import Cookies from "js-cookie";
 import { validate as uuidValidate, v4 as uuidv4 } from "uuid";
-import { imPoweredRequest } from "../components/lib/request";
+import { imPoweredRequest } from "./request";
 
 const analyticsUrl = "http://127.0.0.1:5001/impowered-production/us-central1/analytics/event/page_views";
 
