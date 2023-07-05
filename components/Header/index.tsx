4f45129bccfc707b9e5c7bb10bda7473ac3cc1e6
mondragon-ai <angel.mondragon@yahoo.com>
2023-07-05 13:01:11 -0500

    camhanes funnel

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,24 @@
+import Image from "next/image";
 import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <div className={styles.container}>
-      <h3 className={styles.header}>
-        FREE SHIPPING WHEN YOU BUY 5+ COINS!
-      </h3>
+     <Image src={"https://cdn.shopify.com/s/files/1/0697/5150/5204/files/Win_A_Truck_copy.png?v=1688395242"} width={2000} height={2000} alt={""} style={{height: "100%", width: "auto"}} />
+     <div className={styles.header}>
+        <h3>Join My First Giveawy</h3>
+<br />
+        <p>I wanted to take a moment to personally thank you for joining my first-ever giveaway. Your support and enthusiasm mean the world to me, and I'm thrilled to have you on board.
+<br />
+<br />
+I truly appreciate your participation and believe that our shared journey will be an unforgettable one. Stay tuned for more exciting updates and exclusive content as we embark on this incredible giveaway together.
+<br />
+<br />
+Stay relentless!
+<br />
+<br /></p>
+        <Image src={"https://cdn.shopify.com/s/files/1/0697/5150/5204/files/cam_signature_copy.png?v=1676569649"} width={2000} height={2000} alt={""} style={{height: "24%", width: "auto"}} />
+     </div>
     </div>
   );
 };
--- components/global/Image.tsx
+++ components/global/Image.tsx
@@ -4,7 +4,7 @@ import styles from "../Brand/Brand.module.css";
 const CustomImage = ({ w, h, src }: { w: number;  h: number; src: string }) => {
   return (
     <div className={styles.imgContainer}>
-      <Image src={src} height={h} width={w} alt="" style={{borderRadius: "6px"}} />
+      <Image src={src} height={h} width={w} alt="" style={{borderRadius: "6px", height: "auto"}} />
     </div>
   );
 };
