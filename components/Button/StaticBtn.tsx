356416ff2e7301ee300435cadfee518a92f72a97
mondragon-ai <angel.mondragon@yahoo.com>
2023-07-19 12:01:19 -0500

    static btn

--- components/Button/StaticBtn.tsx
+++ components/Button/StaticBtn.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "../../styles/Home.module.css";
 
 const StaticButton = ({scroll}: any) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window?.scrollY < 7650) {
+        console.log(window?.scrollY);
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window?.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window?.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  if (isScrolled) {
+    return (null);
+  }
   return (
-    <div className={styles.staticButtonContainer} onClick={() => scroll()} >
+   <div className={styles.staticButtonContainer} onClick={() => scroll()} >
       <button className={`${styles.payBtn} ${styles.staticButton}`}>Order Now & Save 30%</button>
-    </div>
+    </div> 
   );
 };
 
